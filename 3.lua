f02c45ee85e6707341f4b0b794b6c0d9d89989ba
fg69873f <dneden6@gmail.com>
2025-10-21 00:52:27 +0700

    Create 3.lua

---
+++ 3.lua
@@ -0,0 +1,9 @@
+local hook
+hook = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local args = {...}
+    if not checkcaller() and getnamecallmethod() == "FireServer" and type(args[1]) == "string" and type(args[2]) == "number" and #args == 2 and #args[1] > 15 then
+        args[2] = 1000000000
+        return hook(self, unpack(args))
+    end
+    return hook(self, ...)
+end))
